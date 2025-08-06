c0f538de12e753da13aefdaba67c41955bd60b39
Sonu Dhakad <148325842+Sonudhakad78@users.noreply.github.com>
2025-08-06 19:56:57 +0530

    third day

--- xyz.js
+++ xyz.js
@@ -1,1 +1,5 @@
 console.log("This is xyz.js file");
+(function (){    //. (module,require) is passed
+    //IIFE - immediately invoked function expression
+    console.log("IIFE");
+})();     // () invoke immediately   
