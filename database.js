294e8db9312c73d4931016d2da7f6c02c0f474c0
Sonu Dhakad <148325842+Sonudhakad78@users.noreply.github.com>
2025-08-20 01:00:54 +0530

    database created

---
+++ database.js
@@ -0,0 +1,49 @@
+const {MongoClient}=require("mongodb");
+
+const url = "mongodb+srv://sonud:senorita@cluster0.xekbnoh.mongodb.net/";
+const client=  new MongoClient(url);
+
+const dbName= "HelloWorld";
+
+async function main(){
+    await client.connect();
+    console.log("connected to database");
+    const db= client.db(dbName);
+    const collection = db.collection('User');
+    
+    //CRUD operations
+    const data = {
+        firstName: "Sonal",
+        lastName: "Dixit",
+        age: "23",
+        city: "Delhi"
+    }
+
+    //insert
+    const insertResult = await collection.insertMany([data]);
+    console.log('Inserted documents =>', insertResult);
+
+    //read
+    const findResult = await collection.find({}).toArray();
+    console.log('Found documents =>', findResult);
+
+    //update
+    const updateResult = await collection.updateMany(
+        {age: "23"},
+        {$set: {age: "24"}}
+    );
+    console.log('Updated documents =>', updateResult);
+
+    //delete
+    const deleteResult = await collection.deleteMany({age:"24"});
+    console.log('Deleted documents =>', deleteResult);
+
+
+    return 'done.';
+}
+
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(()=>client.close());
+
